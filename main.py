4cb88b1ab20eafe3864d91a19edd8f020b9ec729
chastenr <chasten67@yahoo.com>
2025-07-05 13:16:16 +0800

    Connect chatbot to OpenAI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 import os
 
 app = Flask(__name__, static_folder='static')
-
+client = OpenAI()
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
