dc6c3bee7bd53a7f35cc69a6de23d365cd4c98ee
chastenr <chasten67@yahoo.com>
2025-07-05 12:51:06 +0800

    Serve all static files correctly

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder='static')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
